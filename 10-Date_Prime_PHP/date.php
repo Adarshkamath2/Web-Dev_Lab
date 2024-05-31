32fef2cc919ae890914dfa484357b63403ccdd04
Adarsh Kamath <89808317+Adarshkamath2@users.noreply.github.com>
2024-06-01 01:48:48 +0530

    update

--- 10-Date_Prime_PHP/date.php
+++ 10-Date_Prime_PHP/date.php
@@ -5,8 +5,8 @@
    </head>
    <body>
       <?php
-          echo "Today is ".date("d-m-Y")."<br>";
- echo "Today is ".date("l");
+         echo "Today is ".date("d-m-Y")."<br>";
+         echo "Today is ".date("l");
       ?>
    </body>
 </html>
